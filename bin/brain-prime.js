9ba9c3d87003f70184925e39b22ad9190efc40b1
greenboxer1 <greenboxer@list.ru>
2022-12-06 14:11:07 +0300

    add brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,13 +6,13 @@ const userName = greeting();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 for (let i = 0; i < 3; i += 1) {
   const randomNumOne = randomNumber(50);
-  const primeNumers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+  const primeNumers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
   let correctAnswerStr = 'no';
-  for (let primeNumber of primeNumers) {
+  primeNumers.forEach((primeNumber) => {
     if (primeNumber === randomNumOne) {
       correctAnswerStr = 'yes';
     }
-  };
+  });
   if (askAndCheckAnswer(randomNumOne, correctAnswerStr, i, userName) === false) {
     break;
   }
